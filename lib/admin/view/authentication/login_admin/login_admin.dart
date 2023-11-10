cfe548d87635bdfee0dcece9c5646106058905fe
Moazzam Ali (SE) <moazzamali6040@gmail.com>
2023-11-11 01:34:40 +0500

    Add login_admin.dart file

---
+++ lib/admin/view/authentication/login_admin/login_admin.dart
@@ -0,0 +1,205 @@
+import 'package:ecrime/admin/view/authentication/sign_up.dart';
+import 'package:ecrime/client/widgets/generic_text_form_field.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class LoginPageAdmin extends StatefulWidget {
+  const LoginPageAdmin({super.key});
+
+  @override
+  _LoginPageAdminState createState() => _LoginPageAdminState();
+}
+
+class _LoginPageAdminState extends State<LoginPageAdmin> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  bool _loading = false;
+  Future<void> _signIn() async {
+    setState(() {
+      _loading = true;
+    });
+
+    String email = _emailController.text;
+    String password = _passwordController.text;
+
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      User? user = userCredential.user;
+      // Check if the user is an admin
+      if (user != null) {
+        DocumentSnapshot userSnapshot = await FirebaseFirestore.instance
+            .collection('users')
+            .doc(user.uid)
+            .get();
+
+        if (userSnapshot.exists) {
+          bool isAdmin = userSnapshot['isAdmin'] ?? false;
+
+          if (isAdmin) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const AdminHomePage()),
+            );
+          } else {
+            // Display an error message for non-admin users
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: const Text('Access Denied'),
+                  content: const Text(
+                      'You do not have the right permissions to access the admin page.'),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text('OK'),
+                    ),
+                  ],
+                );
+              },
+            );
+          }
+        }
+      }
+    } catch (e) {
+      print('Error during login: $e');
+      // Display an error message
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Login Failed'),
+            content: const Text(
+                'Invalid credentials. Please check your email and password.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    } finally {
+      setState(() {
+        _loading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Admin Login'),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _buildEmailField(),
+                const SizedBox(height: 10),
+                _buildPasswordField(),
+                const SizedBox(height: 20),
+                _buildLoginButton(),
+                const SizedBox(height: 10),
+                _buildSignUpButton(context),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Row _buildSignUpButton(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Don't have an account?"),
+        TextButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SignupPage()),
+            );
+          },
+          child: const Text('Sign Up'),
+        ),
+      ],
+    );
+  }
+
+  ElevatedButton _buildLoginButton() {
+    return ElevatedButton(
+      onPressed: _loading ? null : _signIn,
+      child: _loading ? const CircularProgressIndicator() : const Text('Login'),
+    );
+  }
+
+  GenericTextField _buildPasswordField() {
+    return GenericTextField(
+      controller: _passwordController,
+      labelText: 'Password',
+      hintText: 'Enter your password',
+      prefixIcon: const Icon(Icons.lock),
+      obscureText: true,
+      suffixIcon: const Icon(Icons.visibility),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Password is required';
+        } else if (value.length < 6) {
+          return 'Password must be at least 6 characters';
+        }
+        return null;
+      },
+    );
+  }
+
+  GenericTextField _buildEmailField() {
+    return GenericTextField(
+      controller: _emailController,
+      labelText: 'Email',
+      hintText: 'Enter your email',
+      prefixIcon: const Icon(Icons.mail),
+      keyboardType: TextInputType.emailAddress,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Email is required';
+        } else if (!RegExp(r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$')
+            .hasMatch(value)) {
+          return 'Enter a valid email address';
+        }
+        return null;
+      },
+    );
+  }
+}
+
+class AdminHomePage extends StatelessWidget {
+  const AdminHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Admin Home Page'),
+      ),
+      body: const Center(
+        child: Text('Welcome, Admin!'),
+      ),
+    );
+  }
+}
